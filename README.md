2eb63381dbb6d59768f12b08e1d2e2a5d3eb05ed
Bert <muchia272@gmail.com>
2020-05-22 10:22:11 +0800

    update

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Data Source: [螞蟻金融NLP競賽](https://dc.cloud.alipay.com/index?click_fro
 	- Word2Vec ([使用新聞、百度百科、小說數據來訓練的64維的Word2Vec模型](https://www.floydhub.com/cliffk321/datasets/news_12g_baidubaike_20g_novel_90g_embedding_64bin))
 	- BERT ([bert-as-service](https://github.com/hanxiao/bert-as-service))
 - 計算Sentence Pair的相似度
-- 設定Threshold值，輸出同義/不同義
+- 設定Threshold值，輸出[同義]/[不同義]
 - 比較判斷準確度Accuracy
 
 ![alt text](SentenceSimilarityCalculate.png "TFIDF")
--- SentSimilarBert.py
+++ SentSimilarBert.py
@@ -1,42 +1,11 @@
 from bert_serving.client import BertClient
-import os
-import sys
 from sklearn.metrics.pairwise import cosine_similarity
 import time
-import random
 import numpy as np
 import pandas as pd
-import math
-
-#Have to connect to BERT Server first
-#Using Bert server
-bc = BertClient(check_length=False)
-
-#read file
-dfTest = pd.read_csv('resultHB.csv')
-sents1 = dfTest['sentence1']
-sents2 = dfTest['sentence2']
-dataSize = len(sents1)
-bertSim = ["0" for x in range(dataSize)]
-startTime = time.time()
-
-for i in range(dataSize):
-	#generating BERT sentence Vector
-	sent1 = bc.encode([sents1[i]])
-	sent2 = bc.encode([sents2[i]])
-	#Calculate Similarity
-	sim = cosine_similarity(sent1.reshape(-1, 768), sent2)
-	bertSim[i] = sim[0][0]
-	timeNow = time.time()
-	pastTime = timeNow-startTime
-	print('{} | {:0.1f} | {:0.3f}'.format(i, pastTime, bertSim[i]))
-
-dfTest['bertSimilarity'] = bertSim
-#save file w/ BERT Vector Similarity
-dfTest.to_csv('resultHB.csv', encoding="utf_8_sig", index=False)
 
 
-# calculate label predicting accuracy
+# Calculate label predicting accuracy
 def calAccuracy(df, threshold):
 	label = df['label']
 	sim = df['bertSimilarity']
@@ -50,10 +19,41 @@ def calAccuracy(df, threshold):
 	accuracy = accurateCount/dataSize
 	return accuracy
 
-# read file w/ BERT similarity
-dfResult = pd.read_csv('resultHB.csv', encoding='utf-8')
-# calculate prediction accuracy for each labeling threshold
-for threshold in range(750,1000):
-	acc = calAccuracy(dfResult,threshold)
-	if acc>0.1:
-		print('{:0.3} | {}'.format(acc,threshold/1000))
+if __name__ == "__main__":
+	# Have to connect to BERT Server first
+	# Using Bert server
+	bc = BertClient(check_length=False)
+
+	# Read file
+	dfTest = pd.read_csv('resultHB.csv')
+	sents1 = dfTest['sentence1']
+	sents2 = dfTest['sentence2']
+	dataSize = len(sents1)
+	bertSim = ["0" for x in range(dataSize)]
+	startTime = time.time()
+
+	for i in range(dataSize):
+		# Generating BERT sentence Vector
+		sent1 = bc.encode([sents1[i]])
+		sent2 = bc.encode([sents2[i]])
+		# Calculate Similarity
+		sim = cosine_similarity(sent1.reshape(-1, 768), sent2)
+		bertSim[i] = sim[0][0]
+		timeNow = time.time()
+		pastTime = timeNow-startTime
+		print('{} | {:0.1f} | {:0.3f}'.format(i, pastTime, bertSim[i]))
+
+	dfTest['bertSimilarity'] = bertSim
+	# Save file w/ BERT Vector Similarity
+	dfTest.to_csv('resultHB.csv', encoding="utf_8_sig", index=False)
+
+
+
+
+	# Read file w/ BERT similarity
+	dfResult = pd.read_csv('resultHB.csv', encoding='utf-8')
+	# Calculate prediction accuracy for each labeling threshold
+	for threshold in range(750,1000):
+		acc = calAccuracy(dfResult,threshold)
+		if acc>0.1:
+			print('{:0.3} | {}'.format(acc,threshold/1000))
